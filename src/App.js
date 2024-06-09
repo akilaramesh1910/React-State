6c31bcb59562ba3f2271f11ab6e72ce1b5566267
akila r <akila.r@instavans.com>
2024-06-09 18:12:06 +0530

    state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+  const array =["one", "two", "three"];
+
+  let [state,setState] = useState(array);
+
+  const handleUp = (e) => {
+    const temp = state[0];
+    state[0] = state[1]
+    state[1] = state[2]
+    state[2] = temp
+    const array1 = [state[0],state[1],state[2]]
+    setState(array1)
+  }
+  const handleDown = () => {
+    const temp1 = state[0];
+    const temp2 = state[1];
+    state[0] = state[2]
+    state[1] = temp1
+    state[2] = temp2
+    const array1 = [state[0],state[1],state[2]]
+    setState(array1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>State Change</h2>
+      <div className='container'>
+        <input className='input-field' value={state[0]} readOnly/>
+        <div className='button-container'>
+          <button onClick={handleUp}>+</button>
+          <button onClick={handleDown}>-</button>
+        </div>
+      </div>
+      <div className='container'>
+        <input className='input-field' value={state[1]} readOnly/>
+        <div className='button-container'>
+          <button onClick={handleUp}>+</button>
+          <button onClick={handleDown}>-</button>
+        </div>
+      </div>
+      <div className='container'>
+        <input className='input-field' value={state[2]} readOnly/>
+        <div className='button-container'>
+          <button onClick={handleUp}>+</button>
+          <button onClick={handleDown}>-</button>
+        </div>
+      </div>
     </div>
   );
 }
